e41154ac5e0b41af9c45849c3f20558062443362
Net7xus <157564180+Net7xus@users.noreply.github.com>
2024-01-24 00:44:31 -0500

    Add files via upload

---
+++ search_num.py
@@ -0,0 +1,47 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
+import os
+
+os.system("clear")
+print("        \033[1;34m ____   ____")
+print("        \033[1;34m/  \033[1;37m_ \033[1;34m\ / \033[1;37m_  \033[1;34m\ ")
+print("        \033[1;34m| \033[1;37m|_) \033[1;34m| \033[1;37m(_| \033[1;34m|")
+print("        \033[1;34m|_.__  ___,_|")
+print("             | |")
+print("     __      (_)      __")
+print("    /  \_____| |_____/  \ ")
+print("    |   \033[1;31m|||||||||||||   \033[1;34m|")
+print("     \                 /")
+print("")
+print("   \033[1;31m==>[\033[1;35mCode by: \033[1;37mNet7xus\033[1;31m]<==")
+print("")
+def obtener_informacion(numero):
+    try:
+        numero = phonenumbers.parse(numero)
+        
+        internacional = phonenumbers.format_number(numero, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+        nacional = phonenumbers.format_number(numero, phonenumbers.PhoneNumberFormat.NATIONAL)
+        e164 = phonenumbers.format_number(numero, phonenumbers.PhoneNumberFormat.E164)
+        
+        ubicacion = geocoder.description_for_number(numero, 'es')
+        operador = carrier.name_for_number(numero, 'es')
+        zona_horaria = timezone.time_zones_for_number(numero)
+        
+        print("")
+        os.system("sleep 2")
+        print("\033[1;32m------------------------------------") 
+        print("Información del número:")
+        print("------------------------------------")
+        print("Número Internacional:", internacional)
+        print("Número Nacional:", nacional)
+        print("Número E164:", e164)
+        print("Ubicación:", ubicacion)
+        print("Operador:", operador)
+        print("Zona horaria:", zona_horaria)
+        
+    except phonenumbers.phonenumberutil.NumberParseException:
+        print("\033[1;31mEl número ingresado no es válido.")
+
+
+numero_telefono = input("\033[1;35m[+]Número de tlf (+57): \033[1;37m")
+obtener_informacion(numero_telefono)
